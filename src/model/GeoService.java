1064bee0a98e05eedcabd3e2395b1bfe337085a2
monx.voll <stedy3348112688@gmail.com>
2025-02-19 15:37:09 -0500

    Cambios Finales

--- src/model/GeoService.java
+++ src/model/GeoService.java
@@ -4,7 +4,6 @@ import controller.LocationController;
 
 import javax.swing.*;
 import java.util.List;
-import java.util.Objects;
 
 public class GeoService {
     private LocationController controller;
@@ -13,34 +12,34 @@ public class GeoService {
         this.controller = new LocationController();
     }
 
-
-    public void loadCountries(JComboBox<Country> countryBox){
+    public void loadCountries(JComboBox<Country> countryBox) {
         countryBox.removeAllItems();
-        countryBox.addItem(new Country(0, "Seleccionar")); // Agrega solo al inicio
-        for(Country country : controller.getCountries()){
+        countryBox.addItem(new Country(0, "Seleccionar")); // Opción predeterminada
+
+        List<Country> countries = controller.getCountries();
+        for (Country country : countries) {
             countryBox.addItem(country);
         }
     }
 
-
-
-    public void loadDepartments(JComboBox<Department> departmentBox,JComboBox<City> cityBox,Country selectedCountry){
+    public void loadDepartments(JComboBox<Department> departmentBox, JComboBox<City> cityBox, Country selectedCountry) {
         departmentBox.removeAllItems();
-        departmentBox.addItem(new Department(0, "Seleccionar",0));
+        departmentBox.addItem(new Department(0, "Seleccionar", 0));
         cityBox.removeAllItems();
-        if(selectedCountry!=null){
+
+        if (selectedCountry != null && selectedCountry.getId() != 0) {
             List<Department> departments = controller.getDepartmentsByCountries(selectedCountry.getId());
-            for (Department department : departments){
+            for (Department department : departments) {
                 departmentBox.addItem(department);
             }
         }
     }
 
-
     public void loadCities(JComboBox<City> cityBox, Department selectedDepartment) {
         cityBox.removeAllItems();
-        cityBox.addItem(new City(0,"Seleccionar",0));
-        if (selectedDepartment != null) {
+        cityBox.addItem(new City(0, "Seleccionar", 0));
+
+        if (selectedDepartment != null && selectedDepartment.getId() != 0) {
             List<City> cities = controller.getCitiesByDepartments(selectedDepartment.getId());
             for (City city : cities) {
                 cityBox.addItem(city);
