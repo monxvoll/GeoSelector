20531478a9626e43e8af744aeb1ef125849bf2f9
monx.voll <stedy3348112688@gmail.com>
2025-02-19 15:35:17 -0500

    Logica necesaria para leer las tablas de pais, departamento y ciudad

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -1,23 +1,82 @@
 package database;
 
-import java.sql.*;
+import model.City;
+import model.Country;
+import model.Department;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseConnection {
-    public static void main(String[] args) {
-        String databasePath = "C:/ruta/a/tu/archivo.accdb"; 
-        String url = "jdbc:ucanaccess://" + databasePath;
+    private static final String URL = "jdbc:ucanaccess://resources/DataBase.accdb";
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-        try (Connection conn = DriverManager.getConnection(url)) {
-            System.out.println("Conexión exitosa a Access");
+    public static List<Country> getCountries() {
+        List<Country> countries = new ArrayList<>();
+        String query = "SELECT id, CountryName FROM Country";
 
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM TuTabla");
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
-                System.out.println("ID: " + rs.getInt("ID") + ", Nombre: " + rs.getString("Nombre"));
+                int id = rs.getInt("id");
+                String name = rs.getString("CountryName");
+                countries.add(new Country(id, name));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return countries;
+    }
+
+    public static List<Department> getDepartments(int countryId) {
+        List<Department> departments = new ArrayList<>();
+        String query = "SELECT id, DepartmentName FROM Department WHERE CountryID = " + countryId;
+
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("DepartmentName");
+                departments.add(new Department(id, name, countryId));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return departments;
+    }
+
+    public static List<City> getCities(int departmentId) {
+        List<City> cities = new ArrayList<>();
+        String query = "SELECT id, CityName FROM City WHERE DepartmentID = " + departmentId;
+
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("CityName");
+                cities.add(new City(id, name, departmentId));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cities;
     }
 }
