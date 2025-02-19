fe42b6c1a78792d0604981c8f400f2892af86ae0
monx.voll <stedy3348112688@gmail.com>
2025-02-19 15:36:35 -0500

    Se añade la llamada para la conexion con la base de datos

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
 
+import database.DatabaseConnection;
 import graphicInterface.gui.MainFrame;
+import model.Country;
+
+import java.sql.Connection;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-       MainFrame mainFrame = new MainFrame();
+        Connection conn = DatabaseConnection.getConnection();
+        if (conn != null) {
+            System.out.println("Conexión exitosa a Access");
+        } else {
+            System.out.println("Error al conectar con Access.");
+        }
+
+        MainFrame mainFrame = new MainFrame();
         mainFrame.showWindow();
+
     }
 }
